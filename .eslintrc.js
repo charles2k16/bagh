0ab284f3e6bf0824ceb70b35cd790d2dcfc56d65
charles2k16 <charlesprogram@gmail.com>
2021-09-13 03:02:40 +0000

    single pro

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-v-html": "off"
   },
 }
